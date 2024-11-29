5b6b374dadf247a0f95f1b901898ff3c6fb4749e
Guilherme-Mariano <62659575+Guilherme-Mariano@users.noreply.github.com>
2024-11-29 10:12:57 -0300

    CorrectOptionsAdded

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 3030 || process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.mongo, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -20,18 +20,20 @@ const perguntaSchema = new mongoose.Schema({
   enunciado: { type: String, required: true },
   type: {type:String, required: true},
   options: { type: [String], required: false },
+  correctOption: { type: [String], required: true },
 });
 
 const Pergunta = mongoose.model('Pergunta', perguntaSchema);
 
 app.post('/pergunta', async (req, res) => {
-  console.log(req)
+  console.log(req.body)
   const enunciado = req.body.enunciado
   const type = req.body.type
   const options = req.body.options
+  const correctOption = req.body.checkedOptions
 
-  if (!enunciado || !type) {
-    return res.status(400).json({ erro: 'Enunciado e tipo são obrigatórios, e opções devem ser um vetor de strings.' });
+  if (!enunciado || !type || !correctOption) {
+    return res.status(400).json({ erro: 'Enunciado, tipo e Resposta Correta são obrigatórios.' });
   }
 
   try {
@@ -39,6 +41,7 @@ app.post('/pergunta', async (req, res) => {
       enunciado,
       type,
       options,
+      correctOption,
     });
 
     await novaPergunta.save();
